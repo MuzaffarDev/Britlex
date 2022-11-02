72f3dc5ef87d9d7645b8393469c450c7c7c404c1
MuzaffaR <abdurahmonovmuzaffar56@gmail.com>
2022-11-02 14:39:36 +0500

    headerni menu-itemlariga active klass qo'shildi

--- js/main.js
+++ js/main.js
@@ -5,3 +5,17 @@ document.querySelector(".header__burger-open").addEventListener("click", () => {
 document.querySelector(".header__burger-close").addEventListener("click", () => {
   let header = document.querySelector(".header").classList.remove("show");
 });
+
+// header buttonlari activ  holatga keltiradigan js code
+
+let itemBtn = document.querySelectorAll(".header__menu-item");
+
+itemBtn.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.classList.contains("active")) {
+      item.classList.remove("active");
+    } else {
+      item.classList.add("active");
+    }
+  });
+});
